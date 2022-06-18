f080745a8d9b3c089a0145d89ac9afdd5311d800
Jackson Porter <readjacksonporter@gmail.com>
2022-06-18 09:11:01 -0600

    Second Push

--- arrays.js
+++ arrays.js
@@ -3,12 +3,14 @@
 
 const faveColors = ['red', 'green', 'black']
 
-//CODE HERE
+let colorCopy = faveColors.slice(0,3)
+
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
-//CODE HERE
+colorCopy.push("orange")
+console.log(colorCopy)
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -18,7 +20,7 @@ const faveColors = ['red', 'green', 'black']
 
 const numbers = [1, 2, 3, 4, 5]
 
-//CODE HERE
+let middleNums = numbers.slice(1,4)
 
 
 //////////////////PROBLEM 4////////////////////
@@ -33,4 +35,15 @@ const numbers = [1, 2, 3, 4, 5]
 
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
-// CODE HERE
+let answers = []
+
+for (let i = 0; i < bigOrSmallArray.length; i++) {
+  console.log(bigOrSmallArray[i])
+  if (bigOrSmallArray[i] > 100) {
+    answers.push("big")
+  } else { 
+    answers.push("small")
+  }
+}
+console.log(answers)
+
